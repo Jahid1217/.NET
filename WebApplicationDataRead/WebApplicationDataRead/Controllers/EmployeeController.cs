6ff02dc26fd59c150f5d59e2b1cdacae129e767a
Jahid1217 <jahid.hasan1217@gmail.com>
2025-04-11 00:48:36 +0600

    Database DataShow 06 April,2025

---
+++ WebApplicationDataRead/WebApplicationDataRead/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationDataRead.Context;
+
+namespace WebApplicationDataRead.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            EmployeesEntities _dbContest = new EmployeesEntities();
+            var date = _dbContest.EmployeeTables.ToList();
+            return View(date);
+        }
+    }
+}
