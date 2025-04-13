b3109a80a8062395dec07a1d0de77bf82e290f6f
Jahid1217 <jahid.hasan1217@gmail.com>
2025-04-13 23:45:29 +0600

    Database DataShow And Labtask 13 April,2025

---
+++ WebApplicationdataEntry/WebApplicationdataEntry/Controllers/AddProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationdataEntry.Context;
+
+namespace WebApplicationdataEntry.Controllers
+{
+    public class AddProductController : Controller
+    {
+        // GET: AddProduct
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddProduct()
+        {
+            return View();
+            
+        }
+        [HttpPost]
+        public ActionResult ProductDetails(Product prd)
+        {
+            return View("AddProduct");
+            return Content("Employee Added Successfully");
+        }
+    }
+}
