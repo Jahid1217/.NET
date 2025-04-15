2168306d1cef13f859d1019a4fa5db75298900f0
Jahid1217 <jahid.hasan1217@gmail.com>
2025-04-15 10:30:45 +0600

    Database show and DataInput in dataBase

--- WebApplicationDataRead/WebApplicationDataRead/Controllers/EmployeeController.cs
+++ WebApplicationDataRead/WebApplicationDataRead/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,10 +10,11 @@ namespace WebApplicationDataRead.Controllers
 {
     public class EmployeeController : Controller
     {
+        EmployeesEntities _dbContest = new EmployeesEntities();
         // GET: Employee
         public ActionResult Index()
         {
-            EmployeesEntities _dbContest = new EmployeesEntities();
+           
             var date = _dbContest.EmployeeTables.ToList();
             return View(date);
         }
@@ -24,9 +26,19 @@ namespace WebApplicationDataRead.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult EmployeeInfoSend(EmployeeTable emp)
         {
+            //data entry in database
+            if (ModelState.IsValid)
+            {
+                _dbContest.EmployeeTables.Add(emp);
+                _dbContest.SaveChanges();
+                return RedirectToAction("info"); 
+            }
+
             return View("info");
+            //return View("info");
             //return Content("Employee Added Successfully");
         }
     }
