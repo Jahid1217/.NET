fdbd770cea95f0adc391ace270b6a0e88d5e91cb
Jahid1217 <jahid.hasan1217@gmail.com>
2025-04-18 00:38:41 +0600

    Database show and DataInput in dataBase(Add,delete,Edit(Sir))

--- WebApplicationDataRead/WebApplicationDataRead/Controllers/EmployeeController.cs
+++ WebApplicationDataRead/WebApplicationDataRead/Controllers/EmployeeController.cs
@@ -20,9 +20,20 @@ namespace WebApplicationDataRead.Controllers
         }
 
         [HttpGet]
-        public ActionResult info()
+        public ActionResult info(EmployeeTable emp)
         {
-            return View();
+            if(emp.Id> 0)
+            {
+                return View(emp);
+            }
+            else
+            {
+                ModelState.Clear();
+                
+                return View();
+
+            }
+            //return View(emp);
         }
 
         [HttpPost]
@@ -32,10 +43,45 @@ namespace WebApplicationDataRead.Controllers
             //data entry in database
             if (ModelState.IsValid)
             {
-                _dbContest.EmployeeTables.Add(emp);
-                _dbContest.SaveChanges();
-                TempData["Success"] = "Employee Added Successfully";
-                return RedirectToAction("Index"); 
+                //if(emp.Id > 0)
+                //{
+                //    var empUpdate = _dbContest.EmployeeTables.Where(x => x.Id == emp.Id).FirstOrDefault();
+                //    if (empUpdate != null)
+                //    {
+                //        empUpdate.Name = emp.Name;
+                //        empUpdate.JoingDate = emp.JoingDate;
+                //        empUpdate.Email = emp.Email;
+                //        empUpdate.Call = emp.Call;
+                //        empUpdate.Salay = emp.Salay;
+                //        empUpdate.designation = emp.designation;
+                //    }
+                //}
+                //else
+                //{
+                //    //emp.Id = 0;
+                //}
+                if (emp.Id <= 0)
+                {
+                    var empUpdate = _dbContest.EmployeeTables.Where(x => x.Id == emp.Id).FirstOrDefault();
+                    if (empUpdate != null)
+                    {
+                        this. _dbContest.EmployeeTables.Add(emp);
+                        _dbContest.SaveChanges();
+                        TempData["Success"] = "Employee Added Successfully";
+                        return RedirectToAction("Index");
+                    }
+                }
+                else
+                {
+                    _dbContest.Entry(emp).State = EntityState.Modified;
+                    _dbContest.SaveChanges();
+                    TempData["Success"] = "Employee Updated Successfully";
+                    return RedirectToAction("Index");
+                }
+                //_dbContest.EmployeeTables.Add(emp);
+                //_dbContest.SaveChanges();
+                //TempData["Success"] = "Employee Added Successfully";
+                //return RedirectToAction("Index"); 
             }
 
             return View("info");
@@ -43,25 +89,27 @@ namespace WebApplicationDataRead.Controllers
             //return Content("Employee Added Successfully");
         }
 
-        public ActionResult EmployeeDelete(int id)
+        public ActionResult EmployeeDelete(int? id)
         {
-            var emp = _dbContest.EmployeeTables.Find(id);
+            //var emp = _dbContest.EmployeeTables.Find(id);
+            var emp = _dbContest.EmployeeTables.Where( x => x.Id == id).First();
             if (emp != null)
             {
                 _dbContest.EmployeeTables.Remove(emp);
                 _dbContest.SaveChanges();
+                TempData["remove"] = "Employee remove Successfully";
             }
             return RedirectToAction("Index");
         }
 
-        public ActionResult EmployeeEdit(int id)
-        {
-            var emp = _dbContest.EmployeeTables.Find(id);
-            if (emp != null)
-            {
-                return View(emp);
-            }
-            return RedirectToAction("Index");
-        }
+        //public ActionResult EmployeeEdit(int id)
+        //{
+        //    var emp = _dbContest.EmployeeTables.Find(id);
+        //    if (emp != null)
+        //    {
+        //        return View(emp);
+        //    }
+        //    return RedirectToAction("Index");
+        //}
     }
 }
